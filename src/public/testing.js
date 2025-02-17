b589d7daa9624b05fc0bd66ee3006b565f48a74c
daleymj <daleymj87@gmail.com>
2025-02-17 14:18:02 -0700

    Commit

---
+++ src/public/testing.js
@@ -0,0 +1,8 @@
+$w.onReady(function () {
+    // Write your JavaScript here
+
+    // To select an element by ID use: $w('#elementID')
+
+    // Click 'Preview' to run your code
+    console.log("This is my testing page");
+});
